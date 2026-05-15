08f6baa38ab93c93db92ae8158508ced2894a132
dregun08-rgb <dregun08@gmail.com>
2026-05-15 19:46:17 -0400

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 const nextConfig = {
   images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: 'images.unsplash.com' },
-    ],
+    remotePatterns: [{ protocol: 'https', hostname: 'images.unsplash.com' }],
   },
 };
 module.exports = nextConfig;
